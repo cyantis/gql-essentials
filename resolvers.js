b2c5687b09a5899ee84b74ef55966bbfa3a0b3ca
cyantis <cyantis@gmail.com>
2020-01-03 15:53:23 -0700

    removes middleware

--- resolvers.js
+++ resolvers.js
@@ -1,30 +1,27 @@
 class Friend {
-  constructor(id, { firstName, lastName, language, gender, age, email, contacts }){
-    this.id = id
-    this.firstName = firstName
-    this.lastName = lastName
-    this.language = language
-    this.gender = gender
-    this.age = age
-    this.email = email
-    this.contacts = contacts
-  }
+    constructor(id, { firstName, lastName, gender, age, language, email, contacts }) {
+        this.id = id
+        this.firstName = firstName
+        this.lastName = lastName
+        this.gender = gender
+        this.age = age
+        this.language = language
+        this.email = email
+        this.contacts = contacts
+    }
 }
 
 const friendDatabase = {}
 
-//resolver map
-export const resolvers = {
-  Query: {
+const resolvers = {
     getFriend: ({ id }) => {
-      return new Friend(id, friendDatabase[id])
+        return new Friend(id, friendDatabase[id])
     },
-  },
-  Mutation: {
-    createFriend: ({input}) => {
-      let id = require('crypto').randomBytes(10).toString('hex')
-      friendDatabase[id] = input
-      return new Friend(id, input)
-    },
-  },
+    createFriend: ({ input }) => {
+        let id = require('crypto').randomBytes(10).toString('hex')
+        friendDatabase[id] = input
+        return new Friend(id, input)
+    }
 }
+
+export default resolvers
--- schema.js
+++ schema.js
@@ -1,52 +1,51 @@
-import { resolvers } from './resolvers'
-import { makeExecutableSchema } from 'graphql-tools'
+import { buildSchema } from 'graphql'
 
-const typeDefs = `
-  type Friend {
-    id: ID
-    firstName: String
-    lastName: String
-    language: String
-    gender: Gender
-    age: Int
-    email: String
-    contacts: [Contact]
-  }
+const schema = buildSchema(`
+    type Friend {
+        id: ID
+        firstName: String
+        lastName: String
+        gender: Gender
+        age: Int
+        language: String
+        email: String
+        contacts: [Contact]
+    }
 
-  type Contact {
-    firstName: String
-    lastName: String
-  }
+    type Contact {
+        firstName: String
+        lastName: String
+    }
 
-  input ContactInput {
-    firstName: String
-    lastName: String
-  }
+    enum Gender {
+        MALE
+        FEMALE
+        OTHER
+    }
 
-  enum Gender {
-    FEMALE
-    MALE
-    OTHER
-  }
+    type Query {
+        getFriend(id: ID): Friend
+    }
 
-  type Query {
-    getFriend(id: ID): Friend,
-  }
+    input FriendInput {
+        id: ID
+        firstName: String!
+        lastName: String
+        gender: Gender
+        age: Int
+        language: String
+        email: String!
+        contacts: [ContactInput]
+    }
 
-  input FriendInput {
-    id: ID
-    firstName: String!
-    lastName: String
-    language: String
-    gender: Gender
-    age: Int
-    email: String!
-    contacts: [ContactInput]
-  }
+    input ContactInput {
+        firstName: String
+        lastName: String
+    }
 
-  type Mutation {
-    createFriend(input: FriendInput): Friend
-  }
-`
-const schema = makeExecutableSchema({ typeDefs, resolvers })
-export { schema }
+    type Mutation {
+        createFriend(input: FriendInput): Friend
+    }
+`)
+
+export default schema
